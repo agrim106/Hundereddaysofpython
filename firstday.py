76a566bf3f91a0c3babbf1e4126f61f48306da69
agrim106 <agrimchaudhary2@gmail.com>
2025-01-07 22:27:14 +0530

    Modded this file

--- firstday.py
+++ firstday.py
@@ -1,3 +1,5 @@
 print("Day one");
 print(23);
 print("Agrim is now on his way to learn python.");
+# This is my second day of learning python. 
+# Since I already know java this is pretty easy to learn and understand. 
